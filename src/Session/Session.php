7ae69217f16372a55041c2e716903c7343cbabd2
Didier <dmoindreauu@gmail.com>
2020-10-24 19:35:41 +0200

    Session

--- src/Session/Session.php
+++ src/Session/Session.php
@@ -1,39 +1,83 @@
 <?php
+
 namespace App\Session;
 
+use Exception;
 
 /**
- * 
+ * Class Session
+ * @package App\Session
  */
 class Session
 {
-	
-	public function __construct()
-	{
-		session_start();
-	}
+	public static function start()
+    {
+        session_start();
+    }
 
-	public function hasKey($key)
+    public static function destroy()
+    {
+        session_destroy();
+        unset($_SESSION);
+    }
+
+	public static function has(string $key)
 	{
+        if (!self::exists())
+        {
+            self::start();
+        }
 		return (isset($_SESSION[$key]));
-
-		
 	}
 
-	public function get($key)
+	public static function get(string $key)
 	{
-		if ($this->hasKey($key)) {
+        if (!self::exists())
+        {
+            self::start();
+        }
+		if (self::exists() && self::has($key)) {
 			return $_SESSION[$key];
 		}
 	}
 
-	public function set($key,$value)
+	public static function set(string $key,$value)
 	{
+        if (!self::exists())
+        {
+            self::start();
+        }
 		if (isset($key)) {
 			$_SESSION[$key] = $value;
 		} else {
 			throw new Exception("Error Try to set Session value with undefined Key", 1);
-			
 		}
 	}
+
+	public static function delete(string $key)
+    {
+        if (self::has($key)) {
+            unset($_SESSION[$key]);
+        }
+    }
+
+	public static  function debug()
+    {
+        echo '<pre>';
+        if (self::exists())
+        {
+           dump('PHP_SESSION_ACTIVE');
+        } else {
+            $status = session_status();
+            $statusString = $status === 0 ? 'PHP_SESSION_DISABLED' : 'PHP_SESSION_NONE';
+            dump($statusString);
+        }
+    }
+
+    public static function exists()
+    {
+        return (session_status() === PHP_SESSION_ACTIVE) ? true : false;
+    }
+//Todo add cookies methods
+
 }
