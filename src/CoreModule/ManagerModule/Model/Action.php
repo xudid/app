06302879fafa72321b9054be23affd5aa2a562f7
Didier Moindreau <dmoindreau@gmail.com>
2021-01-16 15:04:54 +0100

    Make Action model compatible with Proxy

--- src/CoreModule/ManagerModule/Model/Action.php
+++ src/CoreModule/ManagerModule/Model/Action.php
@@ -13,112 +13,112 @@ use Entity\Model\Model;
  */
 class Action extends Model
 {
-    /**
-     * @var string
-     * @Column(type="string")
-     */
-    private string $name = '';
-    /**
-     * @var string
-     * @Column(type="string")
-     */
-    private string $type = '';
-    /**
-     * @var string
-     * @Column(type="string")
-     */
-    private string $routeName = '';
-    /**
-     * @var Module|null
-     * @OneToOne(targetEntity="App\CoreModule\ManagerModule\Model\Module")
-     */
-    private ?Module $module;
+	/**
+	 * @var string
+	 * @Column(type="string")
+	 */
+	protected string $name = '';
+	/**
+	 * @var string
+	 * @Column(type="string")
+	 */
+	protected string $type = '';
+	/**
+	 * @var string
+	 * @Column(type="string")
+	 */
+	protected string $routeName = '';
+	/**
+	 * @var Module|null
+	 * @OneToOne(targetEntity="App\CoreModule\ManagerModule\Model\Module")
+	 */
+	protected ?Module $module;
 
-    /**
-     * Action constructor.
-     */
-    public function __construct()
-    {
-        parent::__construct([]);
-    }
+	/**
+	 * Action constructor.
+	 */
+	public function __construct()
+	{
+		parent::__construct([]);
+	}
 
-    /**
-     * @return string
-     */
-    public function getName(): string
-    {
-        return $this->name;
-    }
+	/**
+	 * @return string
+	 */
+	public function getName(): string
+	{
+		return $this->name;
+	}
 
-    /**
-     * @param string $name
-     * @return Action
-     */
-    public function setName(string $name): Action
-    {
-        $this->name = $name;
-        return $this;
-    }
+	/**
+	 * @param string $name
+	 * @return Action
+	 */
+	public function setName(string $name): Action
+	{
+		$this->name = $name;
+		return $this;
+	}
 
-    /**
-     * @return string
-     */
-    public function getType(): string
-    {
-        return $this->type;
-    }
+	/**
+	 * @return string
+	 */
+	public function getType(): string
+	{
+		return $this->type;
+	}
 
-    /**
-     * @param string $type
-     * @return Action
-     */
-    public function setType(string $type): Action
-    {
-        $this->type = $type;
-        return $this;
-    }
+	/**
+	 * @param string $type
+	 * @return Action
+	 */
+	public function setType(string $type): Action
+	{
+		$this->type = $type;
+		return $this;
+	}
 
-    /**
-     * @return string
-     */
-    public function getRouteName(): string
-    {
-        return $this->routeName;
-    }
+	/**
+	 * @return string
+	 */
+	public function getRouteName(): string
+	{
+		return $this->routeName;
+	}
 
-    /**
-     * @param string $routeName
-     * @return Action
-     */
-    public function setRouteName(string $routeName): Action
-    {
-        $this->routeName = $routeName;
-        return $this;
-    }
+	/**
+	 * @param string $routeName
+	 * @return Action
+	 */
+	public function setRouteName(string $routeName): Action
+	{
+		$this->routeName = $routeName;
+		return $this;
+	}
 
-    /**
-     * @return Module|null
-     */
-    public function getModule(): ?Module
-    {
-        return $this->module;
-    }
+	/**
+	 * @return Module|null
+	 */
+	public function getModule(): ?Module
+	{
+		return $this->module;
+	}
 
-    /**
-     * @param Module|null $module
-     * @return Action
-     */
-    public function setModule(?Module $module): Action
-    {
-        $this->module = $module;
-        return $this;
-    }
+	/**
+	 * @param Module|null $module
+	 * @return Action
+	 */
+	public function setModule(?Module $module): Action
+	{
+		$this->module = $module;
+		return $this;
+	}
 
-    /**
-     * @return int
-     */
-    public function getId(): int
-    {
-        return $this->id;
-    }
+	/**
+	 * @return int
+	 */
+	public function getId(): int
+	{
+		return $this->id;
+	}
 }
