748d2cd2cf202a5f423a484e1217fe91325c81f6
Didier Moindreau <dmoindreau@gmail.com>
2021-01-18 18:49:31 +0100

    Add descrition to RouterModule

--- src/CoreModule/RouterModule/RouterModule.php
+++ src/CoreModule/RouterModule/RouterModule.php
@@ -1,28 +1,17 @@
 <?php
 
-
 namespace App\CoreModule\RouterModule;
 
-
-use App\App;
-use App\CoreModule\RouterModule\Controller\RouterController;
 use App\Module\Module;
 
 class RouterModule extends Module
 {
-
-    /**
-     * RouterModule constructor.
-     * @param App $app
-     * @param $controller
-     */
-    public function __construct(App $app)
-    {
-
-    }
-
-    public static function getDir(): string
-    {
-        return __DIR__;
-    }
+	protected static string $name = 'Router';
+	protected static string $description = <<<TEXT
+Provide Application router and route managment
+TEXT;
+	public static function getDir(): string
+	{
+		return __DIR__;
+	}
 }
