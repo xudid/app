dcbca148ed61762f8ac776130e475c45d5c47ffc
Didier Moindreau <dmoindreau@gmail.com>
2021-01-18 18:50:02 +0100

    Add descrition to SetupModule

--- src/CoreModule/SetupModule/SetupModule.php
+++ src/CoreModule/SetupModule/SetupModule.php
@@ -8,10 +8,14 @@ use App\Module\Module;
 
 class SetupModule extends Module
 {
-    // Provide module choice and
-    // some modules can offer cli commands by example RouterModule to generate routesCache list routes ...
-    public static function getDir()
-    {
-        return __DIR__;
-    }
+	// Provide module choice and
+	// some modules can offer cli commands by example RouterModule to generate routesCache list routes ...
+	protected static string $name = 'Setup';
+	protected static string $description = <<<TEXT
+Initialize application database, first account ...
+TEXT;
+	public static function getDir()
+	{
+		return __DIR__;
+	}
 }
