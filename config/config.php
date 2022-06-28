9f4afb9ee265cf2e7b07ac8c2a93fe4d69029fee
Didier <dmoindreauu@gmail.com>
2022-06-28 14:11:49 +0200

    clean config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\CoreModule\AuthorizationModule\Middleware\AuthorizationMiddleware;
 use Middleware\ControllerDispatcher;
 
 return [
@@ -11,41 +10,29 @@ return [
     'site_modules' => 'modules.php',
 
     'core_modules' => [
-        App\CoreModule\SetupModule\SetupModule::class,
         App\CoreModule\LoggingModule\LoggingModule::class,
         App\CoreModule\RouterModule\RouterModule::class,
-        App\CoreModule\AuthModule\AuthModule::class,
-        App\CoreModule\AuthorizationModule\AuthorizationModule::class,
-        App\CoreModule\ManagerModule\ManagerModule::class,
-        App\CoreModule\RoleModule\RoleModule::class,
-        App\CoreModule\UserModule\UserModule::class,
     ],
     'app_modules' => [
-        App\Articles\ArticlesModule::class,
-        App\Stock\StockModule::class,
-        App\Planning\GammeModule\GammeModule::class,
-        App\Planning\PhaseModule\PhaseModule::class,
-        App\Planning\SequencingModule\SequencingModule::class,
     ],
 
     'pipeline' =>[
         'router_middleware',
-        AuthorizationMiddleware::class,
         ControllerDispatcher::class
      ],
 
     'mail_accounts' => [
         'default' => [
-            'host' => '192.168.226.56',
-            'port' => '1025',
-            'user_name' => null,
-            'password' => null,
+            'host' => '',
+            'port' => '',
+            'user_name' => '',
+            'password' => '',
         ]
     ],
     'mail_senders' => [
         'default' => [
-            'email' => 'no-reply@mowjo.fr',
-            'name' => 'no-reply',
+            'email' => '',
+            'name' => '',
         ]
     ]
 
