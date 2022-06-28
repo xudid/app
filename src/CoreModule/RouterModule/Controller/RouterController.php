705302313cdd0fcd0303c9a028275b9790663fbf
Didier <dmoindreauu@gmail.com>
2022-06-28 20:45:41 +0200

    refacto RouterController.php without EntityView

--- src/CoreModule/RouterModule/Controller/RouterController.php
+++ src/CoreModule/RouterModule/Controller/RouterController.php
@@ -1,35 +1,23 @@
 <?php
 
-
 namespace App\CoreModule\RouterModule\Controller;
 
-
 use App\App;
-use Router\Route;
 use Router\Router;
-use SebastianBergmann\CodeCoverage\Node\File;
-use Ui\HTML\Elements\Bases\H3;
-use Ui\HTML\Elements\Nested\Section;
-use Ui\Views\EntityView;
-use Ui\Widgets\FieldInfo;
-use Ui\Widgets\Table\DivTable;
-use Ui\Widgets\Table\TableColumn;
-use Ui\Widgets\Table\TableLegend;
-use Ui\Widgets\Views\Modal;
-use function foo\func;
+use Ui\HTML\Element\Base\H3;
+use Ui\HTML\Element\Nested\Div;
+use Ui\HTML\Element\Nested\Section;
+use Ui\Translation\FileSource;
+use Ui\Translation\Translator;
+use Ui\Widget\Table\ArrayTableFactory;
+use Ui\Widget\Table\Column\Column;
+use Ui\Widget\Table\Legend\TableLegend;
 
 class RouterController
 {
-    /**
-     * @var App
-     */
     private App $app;
     private Router $router;
 
-    /**
-     * RouterController constructor.
-     * @param App $app
-     */
     public function __construct()
     {
         $this->app = App::getInstance();
@@ -48,7 +36,7 @@ class RouterController
     {
         $this->router = $this->app->get('router');
         $route = $this->router->getRoute($method, $routeName);
-        $routeView = new EntityView();
+        $routeView = new Div();
 
         if ($route) {
             $routeView->setTitle('Route ' . $route->getName())->setClass('large-30');
@@ -56,17 +44,15 @@ class RouterController
             $parameters = array_map(function ($name, $pattern){
                 return ['name' => $name, 'pattern' => $pattern];
             }, array_keys($parameters), array_values($parameters));
-            return $routeView->feed(
-                (new FieldInfo('Path', $route->getPath()))->setClass('large-50'),
-                (new DivTable(
-                    [new TableLegend('Parameters', TableLegend::TOP_LEFT)],
-                    [
-                        new TableColumn('name', 'Name'),
-                        new TableColumn('pattern', 'Pattern')
-                    ],
-                    $parameters
-                ))->setClass('large-50')
-            );
+            $source = new FileSource(sys_get_temp_dir() . 'translations.php');
+            $translator = new Translator($source);
+            $tableFactory = new ArrayTableFactory($translator);
+            $tableFactory->setColumns(
+                new Column('name', 'Name'),
+                new Column('pattern', 'Pattern')
+            )->setLegends([new TableLegend('Parameters', TableLegend::TOP_LEFT)])->useData($parameters);
+            return $routeView->feed($tableFactory->getTable())
+                ->setClass('large-50');
         } else {
             $routeView->setTitle('Route not found');
             return $routeView->feed(new H3('No route with this name'));
@@ -75,17 +61,28 @@ class RouterController
 
     private function routesTables()
     {
-        $nameColumn = new TableColumn('name', 'Nom');
+        $nameColumn = new Column('name', 'Nom');
         $scopesTables = [];
         foreach ($this->router->getAuthorizedMethods() as $method) {
-            $routes = $this->router->getRoutes()[$method];
+            $routes = $this->router->getRoutes()[$method] ?? [];
             $methodScopes = array_keys($routes);
+            if (!$methodScopes) {
+                continue;
+            }
             $scopes = [];
             foreach ( $methodScopes as $getScope) {
                 $scopes[]= ['name' => $getScope];
             }
-            $legend = new TableLegend($method,TableLegend::TOP_LEFT);
-            $scopesTables[] = new DivTable([$legend],[$nameColumn],$scopes);
+
+            $legend = new TableLegend($method, TableLegend::TOP_LEFT);
+            $source = new FileSource(sys_get_temp_dir() . 'translations.php');
+            $translator = new Translator($source);
+            $tableFactory = new ArrayTableFactory($translator);
+            $tableFactory->setColumns($nameColumn)
+                         ->setLegends([$legend])
+                         ->useData($scopes);
+
+            $scopesTables[] = $tableFactory->getTable();
         }
 
         return $scopesTables;
