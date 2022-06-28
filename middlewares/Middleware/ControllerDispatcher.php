1c4eac3ce5046ef21671acd0cd6f19bd0b82a564
Didier <dmoindreauu@gmail.com>
2022-06-28 14:14:40 +0200

    clean ControllerDispatcher.php

--- middlewares/Middleware/ControllerDispatcher.php
+++ middlewares/Middleware/ControllerDispatcher.php
@@ -2,7 +2,6 @@
 
 namespace Middleware;
 
-use GuzzleHttp\Psr7\ServerRequest;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -14,23 +13,8 @@ use Psr\Http\Server\RequestHandlerInterface;
  */
 class ControllerDispatcher implements MiddlewareInterface
 {
-
-    /**
-     * @var ResponseInterface
-     */
     private ResponseInterface $response;
 
-    function __construct()
-    {
-
-    }
-
-    /**
-     * Process a Request  and return a Response
-     * @param ServerRequestInterface $request
-     * @param RequestHandlerInterface $handler
-     * @return ResponseInterface
-     */
     function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $this->response = $handler->handle($request);
@@ -47,70 +31,15 @@ class ControllerDispatcher implements MiddlewareInterface
 
         $callback = $route->getCallback();
         $args = $route->getValues();
-        $view = null;
+
         if (sizeof($args) > 0) {
             $view = call_user_func_array($callback, array($args));
         } else {
             $view = call_user_func_array($callback, array());
         }
-        $this->response->getBody()->write($view);
+
+        $this->response->getBody()->write((string)$view);
+
         return $this->response;
-
-        /*if($action != "")
-        {
-
-          $view =null;
-          if(\count($matches)>0)
-          {
-            $view = "";
-            if($controller != null && $action !=null)
-             {
-               $view = $controller->$action($matches);
-             }
-
-             else if(is_string($action))
-             {
-
-               $class = new ReflectionClass($action);
-               if($class->implementsInterface( 'Brick\Action\ActionInterface' )  )
-               {
-                 $a = $class->newInstanceArgs([$this->container]);
-                 $view = $a->call($matches);
-               }
-             }
-             call_user_func_array($callback, array($view,$matches[0]));
-             return $this->response;
-          }
-          //Matches count <=0
-          else
-          {
-            if($controller)
-            {
-              $view = $controller->$action();
-
-            }
-            else if(\is_string($action))
-            {
-              $class = new \ReflectionClass($action);
-              if($class->implementsInterface( 'Brick\Action\ActionInterface' ) )
-              {
-                $a = $class->newInstanceArgs([$this->container]);
-                $view = $a->call();
-              }
-            }
-            call_user_func_array($callback, array($view));
-            return $this->response;
-          }
-
-        }
-        //$action is an empty string
-        else
-        {
-            $response =  call_user_func_array($controller,$matches);
-            if(\is_null($response)){return $this->response;}
-            return $response;
-        }
-
-      }*/
     }
 }
